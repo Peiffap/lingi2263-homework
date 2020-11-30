042eb2c573a8dc46559b1ac495af640dcde01fa9
Gilles Peiffer <gilles.peiffer@outlook.com>
2020-11-30 21:35:32 +0100

    Homework 3: add everything

---
+++ homework3/resources/glove.6B/README.txt
@@ -0,0 +1,1 @@
+Files were too large for GitHub, can be found on the links in the INGInious statements.
---
+++ homework3/script.py
@@ -0,0 +1,7 @@
+import keras
+import tensorflow as tf
+
+
+print(keras.__version__)
+x = tf.constant([[5, 2], [1, 3]])
+print(x)
